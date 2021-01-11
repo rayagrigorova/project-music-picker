28f00dc525d5146089696aa1c83d785ac69673c1
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2021-01-11 15:46:44 +0200

    Step 3 - Initial Functionality

---
+++ Album.java
@@ -0,0 +1,20 @@
+package Project_01_2021_v1;
+
+import java.util.Scanner;
+
+public class Album {
+	String name, artist;
+	int heaviness, mood, energy;
+	
+	Album(){
+		name = "";
+		artist = "";
+		heaviness = 0;
+		mood = 0;
+		energy = 0;
+	}
+	
+	public void print() {
+		System.out.println(name + " " + artist + " " + heaviness + " " + mood + " " + energy + " ");
+	}
+}
--- AlbumChooser.java
+++ AlbumChooser.java
@@ -1,18 +1,33 @@
 package Project_01_2021_v1;
-
 import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
 import javax.swing.*;
+import javax.swing.event.*;
 
 //Extends JPanel so we don't need a JPanel object
 public class AlbumChooser extends JPanel {
-	JLabel purposeLabel, heavinessLabel, energyLabel, moodLabel, noteLabel;
-	JButton giveAlbum, surpriseMe;
-	JCheckBox parentCheckBox;
-	JSlider heavinessSlider, energySlider, moodSlider;
+	private static JLabel purposeLabel, heavinessLabel, energyLabel, moodLabel, noteLabel, resultLabel;
+	private static JButton giveAlbum, surpriseMe;
+	private static JCheckBox parentCheckBox;
+	private static JSlider heavinessSlider, energySlider, moodSlider;
 	//Specifies constraints for components that are laid out using the GridBagLayout.
 	GridBagConstraints gbc = new GridBagConstraints();
+	private static int albumsCount, chosenHeaviness, chosenMood, chosenEnergy;
+	private static boolean parentFriendly;
+	private static Album[] allAlbums;
+	//Most appropriate album given the heaviness, mood and energy
+	private static Album bestAlbum;
 	
 	public AlbumChooser() {
+		setAlbumsCount();
+		setAllAlbums();
+		parentFriendly = false;
+		
 		// A GridBagLayout places components in a grid of rows and columns.
 		//Not all rows necessarily have the same height
 		//and not all columns necessarily have the same width.
@@ -36,6 +51,7 @@ public class AlbumChooser extends JPanel {
 		gbc.gridx = 1;
 		gbc.gridy = 4;
 		add(heavinessSlider, gbc);
+		heavinessSlider.addChangeListener(new HeavinessSliderChange());
 		
 		gbc.gridx = 1;
 		gbc.gridy = 5;
@@ -43,6 +59,7 @@ public class AlbumChooser extends JPanel {
 		add(energyLabel, gbc);
 		energySlider = new JSlider(JSlider.HORIZONTAL,
                 1, 3, 2);
+		energySlider.addChangeListener(new EnergySliderChange());
 		gbc.gridx = 1;
 		gbc.gridy = 6;
 		add(energySlider, gbc);
@@ -53,8 +70,9 @@ public class AlbumChooser extends JPanel {
 		add(moodLabel, gbc);
 		gbc.gridx = 1;
 		gbc.gridy = 8;
-		JSlider moodSlider = new JSlider(JSlider.HORIZONTAL,
+		moodSlider = new JSlider(JSlider.HORIZONTAL,
                 1, 3, 2);
+		moodSlider.addChangeListener(new MoodSliderChange());
 		add(moodSlider, gbc);
 		
 		gbc.gridx = 0;
@@ -62,18 +80,22 @@ public class AlbumChooser extends JPanel {
 	    parentCheckBox = new JCheckBox("Parent-friendly");  
 		parentCheckBox.setBounds(100, 100, 50,50); 
 		add(parentCheckBox, gbc);
+		parentCheckBox.addActionListener(new ParentAction());
 		
 		gbc.gridx = 1;
 		gbc.gridy = 9;
 		giveAlbum = new JButton("Give me an album");  
 	    giveAlbum.setBounds(100, 100, 50,50);  
+	    giveAlbum.addActionListener(new GiveAlbumAction());
 	    add(giveAlbum, gbc);
 		
 		gbc.gridx = 2;
 		gbc.gridy = 9;
 		surpriseMe = new JButton("Surprise me");  
 	    surpriseMe.setBounds(100, 100, 50,50);  
+	    surpriseMe.addActionListener(new SurpriseAction());
 	    add(surpriseMe, gbc);
+	    
 	    
 		gbc.gridx = 0;
 		gbc.gridy = 10;
@@ -82,21 +104,187 @@ public class AlbumChooser extends JPanel {
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 	    noteLabel = new JLabel("*The albums chosen are based on personal preference.");
 		add(noteLabel, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 11;
+		gbc.gridwidth = 3;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		resultLabel = new JLabel("");
+		add(resultLabel, gbc);
+		
+		chosenHeaviness = heavinessSlider.getValue();
+		chosenEnergy = energySlider.getValue();
+		chosenMood = moodSlider.getValue();
+		
 	}
 	
+//	public static void readFile() {
+//		Scanner sc;
+//		sc = new Scanner (AlbumChooser.class.getResourceAsStream("Albums.txt"));
+//		sc.close();
+//	}
+	
+	public int getAlbumsCount() {
+		return albumsCount;
+	}
+	
+	public void setAlbumsCount() {
+		Scanner sc;
+		sc = new Scanner (AlbumChooser.class.getResourceAsStream("Albums.txt"));
+		int ctr = 0;
+		while(sc.hasNext()) {
+			for(int i = 0; i < 5; i++) {
+				sc.nextLine();
+			}
+			ctr++;
+		}
+		albumsCount = ctr;
+		sc.close();
+		return;
+	}
+	public void setAllAlbums() {
+		allAlbums = new Album [albumsCount];
+		Scanner sc = new Scanner(AlbumChooser.class.getResourceAsStream("Albums.txt"));
+		//For each album
+		for(int i = 0; i < albumsCount; i++) {
+			allAlbums[i] = new Album();
+			//For each row
+			for(int j = 0; j < 5 && sc.hasNext(); j++) {
+				switch (j) {
+				case 0: allAlbums[i].name = sc.nextLine(); break;
+				case 1: allAlbums[i].artist = sc.nextLine(); break;
+				case 2: allAlbums[i].heaviness = Integer.parseInt(sc.nextLine()); break;
+				case 3: allAlbums[i].energy = Integer.parseInt(sc.nextLine()); break;
+				case 4: allAlbums[i].mood = Integer.parseInt(sc.nextLine()); break;
+				}
 
+			}
+		}
+		sc.close();
+	}
+	
+	public static class GiveAlbumAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//If the parent-friendly checkbox is chosen, the heaviness should be between 5 and 0
+			if(parentFriendly) {
+				chosenHeaviness = Math.max(chosenHeaviness - 5, 0);
+			}
+			//For each album
+			for(int i = 0; i <  albumsCount; i++) {
+				//We initially set the bestAlbum to the first album
+				if(i == 0) {
+					bestAlbum = allAlbums[i];
+				}
+				//We compare the characteristics with the ones of the best one 
+				//The heaviest albums don't really have a mood or energy
+				else if(compareHeaviness(i) && compareEnergy(i) && compareMood(i) || (chosenHeaviness == 10)) {
+					bestAlbum = allAlbums[i];
+					//If we find a perfect album, we stop searching
+					if(bestAlbum.heaviness == chosenHeaviness &&
+						bestAlbum.energy == chosenHeaviness &&
+						bestAlbum.mood == chosenMood) {
+						break;
+					}
+				}
+			}
+			resultLabel.setText("The best album for you is " + bestAlbum.name + " by " + bestAlbum.artist);
+		}
+		
+	}
+
+	
+	public static class SurpriseAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//We choose a random album from all albums 
+			Random r = new Random();
+			int ind = r.nextInt(albumsCount);
+			
+			do {
+				ind = r.nextInt(albumsCount);
+			}
+			//In case the parent-friendly option is chosen
+			while(parentFriendly && allAlbums[ind].heaviness > 5);
+			
+			resultLabel.setText("A surprise album for you is  " + allAlbums[ind].name + " by " + allAlbums[ind].artist);
+		}
+		
+	}
+	
+	public class ParentAction implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if( parentCheckBox.isSelected()) {
+				parentFriendly = true;
+			}
+			else {
+				parentFriendly = false;
+			}
+			
+		}
+		
+	}
+	
+	public JCheckBox getParentCheckBox() {
+		return this.parentCheckBox;
+	}
+	
+	
+	public class HeavinessSliderChange implements ChangeListener{
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			chosenHeaviness = heavinessSlider.getValue();
+			System.out.println("Heaviness is at " + chosenHeaviness);
+			
+		}
+	}
+	public class EnergySliderChange implements ChangeListener{
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			chosenEnergy = energySlider.getValue();
+			System.out.println("Energy is at " + chosenEnergy);
+			
+		}
+	}	
+	public class MoodSliderChange implements ChangeListener{
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			chosenMood = moodSlider.getValue();
+			System.out.println("Mood is at " + chosenMood);
+			
+		}
+	}	
+	
+	public static boolean compareHeaviness(int i) {
+		return Math.abs(chosenHeaviness - allAlbums[i].heaviness) <= Math.abs(chosenHeaviness - bestAlbum.heaviness);
+	}
+	
+	public static boolean compareEnergy(int i) {
+		return Math.abs(chosenEnergy - allAlbums[i].energy) <= Math.abs(chosenEnergy - bestAlbum.energy);
+	}
+
+	
+	public static boolean compareMood(int i) {
+		return Math.abs(chosenMood - allAlbums[i].mood) <= Math.abs(chosenMood - bestAlbum.mood);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		AlbumChooser a = new AlbumChooser();
 		//Create frame 
 		JFrame frame = new JFrame("Album Chooser");
-		frame.setSize(600, 200);
+		frame.setSize(800, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(a);
-		frame.pack();
+
 		frame.setVisible(true);
+		System.out.println("Albums total:" + a.getAlbumsCount());
 
 	}
+	
 
 }
